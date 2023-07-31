33363f049a36f3657ef20c34983f676b0f42d3a4
dozeran <dozeran@proton.me>
2023-08-01 01:12:46 +0300

    task-1

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,9 @@
-console.log(
-  "Number of categories: " + document.querySelectorAll("li.item").length
-);
+const categories = document.querySelectorAll("#categories .item");
+const categoriesCount = categories.length;
+console.log("Number of categories: ", categoriesCount);
+categories.forEach((element) => {
+  const title = element.firstElementChild;
+  const elementCount = element.lastElementChild.children.length;
+  console.log(`Category: ${title.textContent}`);
+  console.log("Elements:", elementCount);
+});
