ae34edfabad358b93207d9ede9e6384ec6b11868
dozeran <dozeran@proton.me>
2023-08-01 22:12:10 +0300

    task #2, #3

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const list = document.querySelector("#ingredients");
+
+const elements = ingredients.map((element) => {
+  const listEl = document.createElement("li");
+  listEl.textContent = element;
+  listEl.classList.add("item");
+  return listEl;
+});
+
+list.append(...elements);
